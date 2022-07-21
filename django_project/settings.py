505da991b08e114e0c9fde5815698799866ad302
jayjaychukwu <liljaydon.pj@gmail.com>
2022-07-21 22:52:22 +0100

    add environment variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = Env()
+env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
